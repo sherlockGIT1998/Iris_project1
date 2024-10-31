8fd6038eaadb53e4971330d830e0eb5412ad121e
sherlockGIT1998 <bardeanu21@gmail.com>
2024-11-01 00:05:22 +0530

    All files are uploaded

---
+++ config.py
@@ -0,0 +1,3 @@
+MODEL_FILE_PATH = r'D:\Deloitte_Task\40_SunnyDeshmukh_Iris_Dataset\project_app\logistc_model_Iris_Dataset.pkl'
+
+JSON_FILE_PATH = r'D:\Deloitte_Task\40_SunnyDeshmukh_Iris_Dataset\project_app\Project_Data_iris_Dataset.json'
